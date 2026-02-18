8b21d6bb66bf18e7640a85e9ac6e39ef5e8f7ffe
Mourad Ouarab <mou.ouarab.25@lehre.mosbach.dhbw.de>
2026-02-18 10:29:44 +0100

    Implement matrix multiplication in MatrixMult.java

---
+++ Programming Semester 1/MatrixMult.java
@@ -0,0 +1,39 @@
+public class MatrixMulti {
+    public static void main(String[] args) {
+        int[][] A = {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+
+        int[][] B = {
+            {7, 8},
+            {9, 10},
+            {11, 12}
+        };
+
+     
+        int rowsA = A.length;       
+        int colsA = A[0].length;   
+        int colsB = B[0].length;   
+        int[][] C = new int[rowsA][colsB];
+
+        // Matrix multiplication
+        for (int i = 0; i < rowsA; i++) {
+            for (int j = 0; j < colsB; j++) {
+                C[i][j] = 0; 
+                for (int k = 0; k < colsA; k++) {
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+
+        // Print result matrix
+        System.out.println("Result:");
+        for (int[] row : C) {
+            for (int value : row) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
+        }
+    }
+}
