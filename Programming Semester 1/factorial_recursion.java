3ed1153e7b234467655afe561d97e4a827cdfca2
Mourad Ouarab <mou.ouarab.25@lehre.mosbach.dhbw.de>
2026-02-18 10:31:48 +0100

    Add recursive factorial implementation in Java

---
+++ Programming Semester 1/factorial_recursion.java
@@ -0,0 +1,20 @@
+    public class factorial_recursion{
+
+    public static void main(String[] args) {
+        int number = 4;
+
+        if (number < 0) {
+            System.out.println("Factorial is not defined for negative numbers.");
+        } else {
+            int result = factorial(number);
+            System.out.println(number + "! = " + result);
+        }
+    }
+
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1; 
+        }
+        return n * factorial(n - 1); 
+    }
+}
